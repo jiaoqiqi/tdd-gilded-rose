39dcdef9b68ffe6f31c401bc436fe4e0f994d1f4
jiaoqiqi <jiaoqiqi977@163.com>
2020-05-12 21:31:45 +0800

    Make test could pass in env and delete extra file :pear: Qilin
    Signed-off-by: jiaoqiqi <jiaoqiqi977@163.com>

---
+++ src/test/java/cn/xpbootcamp/gildedrose/RoseTest.java
@@ -0,0 +1,15 @@
+package cn.xpbootcamp.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RoseTest {
+
+
+    @Test
+    void should_return_1_when_given_1() {
+        assertThat("1").isEqualTo("1");
+    }
+}
+
