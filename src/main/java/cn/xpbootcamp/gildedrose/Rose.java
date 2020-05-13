8dc2555fafd75d8004741671002958438d780e42
Phony Lou <louqilin555@gmail.com>
2020-05-13 16:57:21 +0800

    Refactor: Remove type checking logic

--- src/main/java/cn/xpbootcamp/gildedrose/Rose.java
+++ src/main/java/cn/xpbootcamp/gildedrose/Rose.java
@@ -1,32 +1,10 @@
 package cn.xpbootcamp.gildedrose;
 
-import cn.xpbootcamp.gildedrose.model.AgedBrie;
-import cn.xpbootcamp.gildedrose.model.BackstagePass;
-import cn.xpbootcamp.gildedrose.model.CommonGoods;
 import cn.xpbootcamp.gildedrose.model.Goods;
 
 public class Rose {
-    Goods refresh(Goods goods) {
-
-        if (goods instanceof AgedBrie) {
-            Goods refreshedGoods = goods.refreshQuality();
-            Goods result = refreshedGoods.doQualityLimitation();
-            return result;
-        }
-        if (goods instanceof BackstagePass) {
-
-            Goods refreshedGoods = goods.refreshQuality();
-            Goods result = refreshedGoods.doQualityLimitation();
-            return result;
-        }
-        if (goods instanceof CommonGoods) {
-            Goods refreshedGoods = goods.refreshQuality();
-            Goods result = refreshedGoods.doQualityLimitation();
-            return result;
-        }
-
-        Goods result = goods.doQualityLimitation();
-
-        return result;
+    Goods refresh(final Goods goods) {
+        final Goods refreshedGoods = goods.refreshQuality();
+        return refreshedGoods.doQualityLimitation();
     }
 }
