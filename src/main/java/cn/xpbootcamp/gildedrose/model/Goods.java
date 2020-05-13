a4fa45283431476366c12812603f0550085fe391
Phony Lou <louqilin555@gmail.com>
2020-05-13 11:37:06 +0800

    Refactor package structure

---
+++ src/main/java/cn/xpbootcamp/gildedrose/model/Goods.java
@@ -0,0 +1,23 @@
+package cn.xpbootcamp.gildedrose.model;
+
+public class Goods {
+    private int sellIn;
+    private int quality;
+
+    public Goods(int sellIn, int quality) {
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+}
---
+++ src/main/java/cn/xpbootcamp/gildedrose/model/Sulfuras.java
@@ -0,0 +1,8 @@
+package cn.xpbootcamp.gildedrose.model;
+
+public class Sulfuras extends Goods{
+
+    public Sulfuras(int sellIn, int quality) {
+        super(sellIn, quality);
+    }
+}
