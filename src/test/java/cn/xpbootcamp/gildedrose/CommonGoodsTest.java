40ca424b1d755a5d4f09d8d96b550d1624120893
jiaoqiqi <jiaoqiqi977@163.com>
2020-05-17 06:53:19 +0800

    Should decreate 2 quality when sellIn less than 0
    Signed-off-by: jiaoqiqi <jiaoqiqi977@163.com>

--- src/test/java/cn/xpbootcamp/gildedrose/CommonGoodsTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/CommonGoodsTest.java
@@ -15,4 +15,11 @@ public class CommonGoodsTest {
         assertThat(updatedAgedBrie.getSellIn()).isEqualTo(9);
         assertThat(updatedAgedBrie.getQuality()).isEqualTo(19);
     }
+    @Test
+    void should_sellIn_minus_2_quality_4_when_one_day_passed_given_sellIn_minus_1_quality_6() {
+        CommonGoods commonGoods = new CommonGoods(-1,6);
+        Goods updatedAgedBrie = commonGoods.refreshQuality();
+        assertThat(updatedAgedBrie.getSellIn()).isEqualTo(-2);
+        assertThat(updatedAgedBrie.getQuality()).isEqualTo(4);
+    }
 }
